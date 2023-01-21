1039642678114cf28a497ca8ef1b6b2800d48296
naeder311 <114816929+naeder311@users.noreply.github.com>
2023-01-21 15:15:48 -0500

    Update ContactForm.js

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 import emailjs from '@emailjs/browser';
 import { useRef } from 'react';
 import email from '../assets/images/email.svg';
-import { useNavigate, Link, Navigate } from 'react-router-dom';
+import { useNavigate, Navigate } from 'react-router-dom';
 
 const ContactInfo = () => {
   const navigate = useNavigate();
